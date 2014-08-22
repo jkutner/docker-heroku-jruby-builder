ae9839a254ccc05510a6e47014501e6376a61673
Terence Lee <hone02@gmail.com>
2014-08-22 15:47:42 -0500

    use the cedar-14 tag for building jrubies

--- rubies/ruby-1.8.7-jruby-1.7.13.sh
+++ rubies/ruby-1.8.7-jruby-1.7.13.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker.io run -v `pwd`/builds:/tmp/output -v `pwd`/../cache:/tmp/cache -e VERSION=1.7.13 -e RUBY_VERSION=1.8.7 -t hone/jruby-builder
+docker.io run -v `pwd`/builds:/tmp/output -v `pwd`/../cache:/tmp/cache -e VERSION=1.7.13 -e RUBY_VERSION=1.8.7 -t hone/jruby-builder:cedar-14
